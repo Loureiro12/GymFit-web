44d2a5a8380017175c7296328bd00a3efc51f596
André Loureiro <65689062+Loureiro12@users.noreply.github.com>
2024-01-07 11:57:27 -0300

    feat: configuring project routes

---
+++ src/routes.tsx
@@ -0,0 +1,15 @@
+import { createBrowserRouter } from 'react-router-dom'
+
+import { Home } from './pages/app/home'
+import { SignIn } from './pages/auth/sign-in'
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/sign-in',
+    element: <SignIn />,
+  },
+])
