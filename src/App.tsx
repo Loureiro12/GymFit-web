157062118be4e81c0240e0188194b93efc5f35c0
André Loureiro <65689062+Loureiro12@users.noreply.github.com>
2024-01-07 12:15:21 -0300

    feat: creating standard layout for the application

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
-import { RouterProvider } from 'react-router-dom'
 import './global.css'
+
+import { RouterProvider } from 'react-router-dom'
+import { Helmet, HelmetProvider } from 'react-helmet-async'
+
 import { router } from './routes'
 
 export function App() {
-  return <RouterProvider router={router} />
+  return (
+    <HelmetProvider>
+      <Helmet titleTemplate="%s | GymFit" />
+
+      <RouterProvider router={router} />
+    </HelmetProvider>
+  )
 }
