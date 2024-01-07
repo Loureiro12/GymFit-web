938db6d6731ac8e735532bc4a6b8a009d384fd60
André Loureiro <65689062+Loureiro12@users.noreply.github.com>
2024-01-07 12:51:39 -0300

    feat: creating login screen

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -1,13 +1,21 @@
 import { Outlet } from 'react-router-dom'
+import { Headphones } from 'lucide-react'
 
 export function AuthLayout() {
   return (
-    <div>
-      <h1>Autenticação</h1>
+    <div className="flex min-h-screen flex-col items-center justify-between p-8">
+      <div className="flex items-center gap-3 text-lg  text-foreground">
+        <Headphones className="h-5 w-5" />
+        <span className="font-semibold">GymFit</span>
+      </div>
 
-      <div>
+      <div className="">
         <Outlet />
       </div>
+
+      <footer>
+        Painel do parceiro &copy; gymFit - {new Date().getFullYear()}
+      </footer>
     </div>
   )
 }
--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,10 +1,40 @@
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
 import { Helmet } from 'react-helmet-async'
 
 export function SignIn() {
   return (
     <>
       <Helmet title="Login" />
-      <h1>SignIn</h1>
+      <div>
+        <div className="flex flex-col justify-center gap-6">
+          <div className="flex flex-col gap-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Acessar painel
+            </h1>
+            <p className="text-sm text-muted-foreground">
+              Acesse o painel para fazer o acompanhamento dos seus alunos!
+            </p>
+          </div>
+
+          <form className="space-y-4">
+            <div className="space-y-2">
+              <Label htmlFor="email">Email</Label>
+              <Input id="email" type="email" />
+            </div>
+
+            <div className="space-y-2">
+              <Label htmlFor="password">Senha</Label>
+              <Input id="password" type="password" />
+            </div>
+
+            <Button type="submit" className="w-full">
+              Entrar
+            </Button>
+          </form>
+        </div>
+      </div>
     </>
   )
 }
